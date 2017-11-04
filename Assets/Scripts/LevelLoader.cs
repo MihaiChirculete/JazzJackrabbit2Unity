763b2c2e59cf6da1d6c7aca5bb1c893f4fa41f89
MihaiChirculete <mihai.chirculete98@gmail.com>
2017-11-04 12:08:54 +0200

    Added tileset loading, level data loading and tile generation.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -112,41 +112,11 @@ public class LevelLoader : MonoBehaviour {
 
 		string tileset = System.Text.Encoding.UTF8.GetString(d1_lvl.Tileset).TrimEnd('\0').ToLower().Replace(".j2t", "");
 
-		/*
-		for(uint layer=0; layer<8; layer++)
-		{
-			if(d1_lvl.DoesLayerHaveAnyTiles[layer])
-			{
-				uint yPos = d1_lvl.LayerHeight[layer];
-				for(uint i=0; i<d1_lvl.LayerHeight[layer]; i++)
-				{
-					for(uint j=0; j<d1_lvl.LayerWidth[layer]; j++)
-					{
-						J2L_Tile tile = Utils.GetTile(lh, d1_lvl, d3_lvl, d4_lvl, ref TileFrame, layer, j, i, 0f);
-						
-						Debug.Log("Prefabs/Tiles/" + tileset + "/" + tileset + "_" + tile.index);
-
-						if(tile.flipped)
-							Debug.Log("Tile is flipped");
-
-						
-						GameObject tileObj = (GameObject)Instantiate(Resources.Load("Prefabs/Tiles/" + tileset + "/" + tileset + "_" + tile.index));
-						tileObj.transform.position = new Vector3(j, yPos, layer);
-					}
-
-					yPos--;
-				}
-			}
-		}
-
-		TileCoord playerSpawnPos = Utils.GetPlayerStart(d1_lvl, d2_lvl, false);
-		player.SetActive(true);
-		// player.transform.position = new Vector3(playerSpawnPos.x, d1_lvl.LayerHeight[3] - playerSpawnPos.y, 2);
-		player.transform.position = new Vector3(25, 50, 2);
+		
+		
 
 		GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("Music/" + System.Text.Encoding.UTF8.GetString(d1_lvl.MusicFile).TrimEnd('\0').ToLower());
 		GetComponent<AudioSource>().PlayDelayed(0f);
-		*/
 
 		tileTextures = new Texture2D[tilesetObj.TilesetInfo_1_23.TileCount];
 
@@ -159,8 +129,9 @@ public class LevelLoader : MonoBehaviour {
 		//thrd = new Thread(GenerateTileTextures);
 		//thrd.Start();
 
-		DrawPalette();
+		// DrawPalette();
 		GenerateTileTextures();
+		GenerateLevel();
 
 	}
 	
@@ -180,26 +151,28 @@ public class LevelLoader : MonoBehaviour {
 			uint[] pixels = tilesetObj.GetTile((uint)i, true);
 
 			if(pixels.Length == 1024)
+			
 			for (int y = 0; y < 32; y++)
 	        {
 	            for (int x = 0; x < 32; x++)
 	            {
 	            	//Debug.Log("paletteColor index: " + (32 * y + x));
 	            	uint pixel = pixels[32*y + x];
-	            	Color32 c = new Color32((byte)Utils.getByteFromNumber(pixel, 1),
-	            		(byte)Utils.getByteFromNumber(pixel, 2),
-	            		(byte)Utils.getByteFromNumber(pixel, 3),
-	            		(byte)Utils.getByteFromNumber(pixel, 4));
+	            	Color32 c = new Color32((byte)(Utils.getByteFromNumber(pixel, 1)),// & 0xFF000000),
+	            		(byte)(Utils.getByteFromNumber(pixel, 2)), // & 0x00FF0000),
+	            		(byte)(Utils.getByteFromNumber(pixel, 3)), // & 0x0000FF00),
+	            		(byte)(Utils.getByteFromNumber(pixel, 4)));// & 0x000000FF));
 
 	            	tileTextures[i].SetPixel(32-x, 32-y, c);
 	            }
 	        }
+	        
 
 	        tileTextures[i].Apply();
 
-	        GameObject tileObj = (GameObject)Instantiate(Resources.Load("Prefabs/Tile/GenericTile"));
-			tileObj.transform.position = new Vector3(i, 0, 0);
-			tileObj.GetComponent<SpriteRenderer>().sprite = Sprite.Create(tileTextures[i], new Rect(0.0f, 0.0f, 32f, 32f), new Vector2(0.5f, 0.5f), 32f);
+	        //GameObject tileObj = (GameObject)Instantiate(Resources.Load("Prefabs/Tile/GenericTile"));
+			//tileObj.transform.position = new Vector3(i, 0, 0);
+			//tileObj.GetComponent<SpriteRenderer>().sprite = Sprite.Create(tileTextures[i], new Rect(0.0f, 0.0f, 32f, 32f), new Vector2(0.5f, 0.5f), 32f);
 		}
 
 		//Thread.CurrentThread.Abort();
@@ -223,5 +196,37 @@ public class LevelLoader : MonoBehaviour {
 		paletteTexture.Apply();
 
 		tileObj.GetComponent<SpriteRenderer>().sprite = Sprite.Create(paletteTexture, new Rect(0.0f, 0.0f, 256f, 256f), new Vector2(0.5f, 0.5f), 32f);
+	}
+
+	void GenerateLevel()
+	{
+		for(uint layer=0; layer<8; layer++)
+		{
+			if(d1_lvl.DoesLayerHaveAnyTiles[layer])
+			{
+				uint yPos = d1_lvl.LayerHeight[layer];
+				for(uint i=0; i<d1_lvl.LayerHeight[layer]; i++)
+				{
+					for(uint j=0; j<d1_lvl.LayerWidth[layer]; j++)
+					{
+						J2L_Tile tile = Utils.GetTile(lh, d1_lvl, d3_lvl, d4_lvl, ref TileFrame, layer, j, i, 0f);
+
+						if(tile.flipped)
+							Debug.Log("Tile is flipped");
+
+						GameObject tileObj = (GameObject)Instantiate(Resources.Load("Prefabs/Tile/GenericTile"));
+						tileObj.GetComponent<SpriteRenderer>().sprite = Sprite.Create(tileTextures[tile.index], new Rect(0.0f, 0.0f, 32f, 32f), new Vector2(0.5f, 0.5f), 32f);
+						tileObj.transform.position = new Vector3(j, yPos, layer);
+					}
+
+					yPos--;
+				}
+			}
+		}
+
+		TileCoord playerSpawnPos = Utils.GetPlayerStart(d1_lvl, d2_lvl, false);
+		//player.SetActive(true);
+		// player.transform.position = new Vector3(playerSpawnPos.x, d1_lvl.LayerHeight[3] - playerSpawnPos.y, 2);
+		//player.transform.position = new Vector3(25, 50, 2);
 	}
 }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Port of the Jazz Jackrabbit 2 game by EpicMegaGames written in C# for the Unity game engine.
 
 (Work in progress)
+
+# What currently works
+    - Loading of tilesets (mostly, for some reason the images generated are purple)
+    - Loading the level data and generating the tiles (mostly)
